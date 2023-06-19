7da8950e6ecae6df731c40f8213977aa9aba86ab
BMKGKOTIM <bmkgkotim@gmail.com>
2023-06-19 23:50:27 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import 'aos/dist/aos.css';
 import "./App.css";
 import Home from "./pages/home";
 import Header from "./components/header";
-import { useEffect } from "react";
 
 function App() {
   AOS.init()
@@ -15,16 +14,6 @@ function App() {
     width:"100%",
     height:"100vh",
   }
-  const description = `
-    Kurniawan rizki trinanta sembiring, personal website/portofolio website programming enthusiast programming competitive
-  `
-  useEffect(() => {
-    var meta = document.createElement('meta');
-    meta.setAttribute('name', 'description');
-    meta.setAttribute('content', description);
-    document.getElementsByTagName('head')[0].appendChild(meta);
-    
-  } ,[])
 
   return (
     <>
