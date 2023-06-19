61a7f13bb723adb2f6037a20112e624708c42a52
BMKGKOTIM <bmkgkotim@gmail.com>
2023-06-19 23:45:20 +0700

    adding content met

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import 'aos/dist/aos.css';
 import "./App.css";
 import Home from "./pages/home";
 import Header from "./components/header";
+import { useEffect } from "react";
 
 function App() {
   AOS.init()
@@ -14,6 +15,15 @@ function App() {
     width:"100%",
     height:"100vh",
   }
+  const description = `
+    Kurniawan rizki trinanta sembiring, personal website/portofolio website programming enthusiast programming competitive
+  `
+  useEffect(() => {
+    var meta = document.createElement('meta');
+    meta.setAttribute('name', 'description');
+    meta.setAttribute('content', description);
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  } ,[])
 
   return (
     <>
